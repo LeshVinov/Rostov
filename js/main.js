92aca7276069ea6845959668db44e5504509d774
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2022-01-27 10:11:39 +0300

    feat: add jquery code for modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function (event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -24,5 +25,33 @@ document.addEventListener("DOMContentLoaded", function (event) {
             };
         };
     });
-
 })
+*/
+
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+
+    var modalDialog = $('.modal__dialog')
+
+    $(document).mouseup( function(e) {
+        if(!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0 ) {
+            modal.removeClass('modal--visible')
+        };
+    });
+    $(document).on('keydown', function(e) {
+        if (e.keyCode == 27)
+            modal.removeClass('modal--visible');
+        });
+});
+
+
+
